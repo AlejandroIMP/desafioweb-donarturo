76bcc68bbf3955b56e117db70c22d6650c0d6ba8
alejandrotsx <alejandrosunuc@gmail.com>
2024-12-21 13:16:49 -0600

    add authentication features with login, registration, and token verification

--- backend/src/routes/products.routes.js
+++ backend/src/routes/products.routes.js
@@ -1,18 +1,14 @@
 import { Router } from "express";
 import { getProducts, getProductById, createProduct, updateProduct, deleteProduct, updateProductState } from "../controllers/products.controller.js";
+import { verifyToken } from "../middleware/auth.js";
 
 const router = Router();
 
-router.get('/productos', getProducts);   
-
-router.get('/productos/:id', getProductById);
-
-router.post('/productos', createProduct);
-
-router.put('/productos/:id', updateProduct);
-
-router.patch('/productos/:id', updateProductState);
-
-router.delete('/productos/:id', deleteProduct);
+router.get('/productos', verifyToken, getProducts);
+router.get('/productos/:id', verifyToken, getProductById);
+router.post('/productos', verifyToken, createProduct);
+router.put('/productos/:id', verifyToken, updateProduct);
+router.patch('/productos/:id', verifyToken, updateProductState);
+router.delete('/productos/:id', verifyToken, deleteProduct);
 
 export default router;
