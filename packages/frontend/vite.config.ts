3e59ecd4e65b60dd4262ce85945b873cfe02890f
alejandrotsx <alejandrosunuc@gmail.com>
2024-12-30 12:59:45 -0600

    feat: add React SVG icon and configure path aliases in TypeScript and Vite

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import * as path from 'path'
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      { find: '@components', 
+        replacement: path.resolve(path.join(__dirname, '/src/components')) },
+    ],
+  },
+  build: {
+    // generate .vite/manifest.json in outDir
+    manifest: true,
+    rollupOptions: {
+      // overwrite default .html entry
+      input: '/path/to/main.js',
+    },
+  },
+  server: {
+    origin: 'http://localhost:5000',
+  },
 })
