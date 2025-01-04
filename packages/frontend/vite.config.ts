a353192ba6acebf87800c07ba4ec910ed937d3b9
alejandrotsx <alejandrosunuc@gmail.com>
2025-01-03 22:39:42 -0600

    feat: update project structure and dependencies for improved module resolution and styling

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -2,15 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import * as path from 'path'
 
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [
-      { find: '@components', 
-        replacement: path.resolve(path.join(__dirname, '/src/components')) },
-    ],
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
   },
   build: {
     // generate .vite/manifest.json in outDir
