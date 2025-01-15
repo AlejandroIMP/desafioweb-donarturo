2eb9aea7678a45e2c9d25aec01fa572a67531caa
alejandrotsx <alejandrosunuc@gmail.com>
2025-01-15 15:26:59 -0600

    feat: implement product info modal in Card component with state management

--- packages/frontend/src/components/Card/index.tsx
+++ packages/frontend/src/components/Card/index.tsx
@@ -1,7 +1,11 @@
-import { IProduct, CartProduct } from "@/interfaces/product.interface"
+import { DataProduct, CartProduct } from "@/interfaces/product.interface"
+import ProductInfoModal from '@/components/ProductInfoModal';
+import { Dialog, IconButton } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
 import { useClientContext } from "@/hooks";
 import AddIcon from '@mui/icons-material/Add';
 import CheckIcon from '@mui/icons-material/Check';
+import { useState } from 'react';
 import './index.css';
 
 interface CardProps {
@@ -11,9 +15,21 @@ interface CardProps {
 const Card = ({ product }: CardProps) => {
   const { cartProducts, count, setCount, openCheckoutSideMenuHandler, addProduct } = useClientContext();
 
+  const [openModalProd, setOpenModalProd] = useState(false);
+
+  const handleCloseModalProd = () => {
+    setOpenModalProd(false);
+  };
+
+  const handleOpenModalProd = () => {
+    setOpenModalProd(true);
+  };
+
+
   interface AddProductToCartEvent extends React.MouseEvent<HTMLElement> { }
 
-  const addProductToCart = (event: AddProductToCartEvent, product: IProduct): void => {
+  const addProductToCart = (event: AddProductToCartEvent, product: DataProduct): void => {
+    event.stopPropagation();
     event.preventDefault();
     addProduct(product);
     setCount(count + 1);
@@ -51,19 +67,44 @@ const Card = ({ product }: CardProps) => {
   }
 
   return (
-    <div className="card--container">
-      <figure>
-        {renderIcon()}
-        <img src={product.foto} alt={product.nombre} />
-        <figcaption>
-          <h3>{product.nombre}</h3>
-        </figcaption>
-      </figure>
-      <div>
-        <p>${product.precio.toFixed(2)}</p>
-        <p>{product.marca}</p>
+    <>
+
+      <div className="card--container" onClick={handleOpenModalProd}>
+        <figure>
+          {renderIcon()}
+          <img src={product.foto} alt={product.nombre} />
+          <figcaption>
+            <h3>{product.nombre}</h3>
+          </figcaption>
+        </figure>
+        <div>
+          <p>${product.precio.toFixed(2)}</p>
+          <p>{product.marca}</p>
+        </div>
       </div>
-    </div>
+      <Dialog
+        open={openModalProd}
+        onClose={handleCloseModalProd}
+        maxWidth="md"
+        fullWidth
+      >
+        <div style={{
+          display: 'flex',
+          justifyContent: 'flex-end',
+          padding: '8px'
+        }}>
+          <IconButton
+            onClick={handleCloseModalProd}
+            size="small"
+          >
+            <CloseIcon />
+          </IconButton>
+        </div>
+        <div>
+          <ProductInfoModal product={product} />
+        </div>
+      </Dialog>
+    </>
   )
 }
 
