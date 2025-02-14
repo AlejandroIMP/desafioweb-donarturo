56e5b7a95927c015c667af37db39de48ecab7106
alejandrotsx <alejandrosunuc@gmail.com>
2025-02-13 22:54:45 -0600

    fix: update Vite configuration to use correct input path and enable HMR

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,7 +1,8 @@
+/// <reference types="vite/client" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import * as path from 'path'
-import { env } from 'process'
+// import { env } from 'process'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -16,10 +17,11 @@ export default defineConfig({
     manifest: true,
     rollupOptions: {
       // overwrite default .html entry
-      input: path.resolve(__dirname, 'src', 'main.tsx'),
+      input: path.resolve(__dirname, 'index.html'),
     },
   },
   server: {
-    origin: env.VITE_API_BASE_URL_CONFIG,
+    // origin: env.VITE_API_BASE_URL_CONFIG,
+    hmr: true,
   },
 })
